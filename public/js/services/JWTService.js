44bcfe313355144aa49494cbd01193ddec132c9b
Isaac Anyimiah <iandev096@gmail.com>
2022-10-04 19:42:48 +0000

    added logic for JWTService

---
+++ public/js/services/JWTService.js
@@ -0,0 +1,116 @@
+const JWT_KEY = "__HAHA__ ";
+
+export class JWTService {
+  #accessSubscriberFns = [];
+  #refreshSubscriberFns = [];
+  #accessTimeout;
+  #refreshTimeout;
+
+  constructor() {
+    const jwtData = this.get();
+    if (jwtData) {
+      this.initialize(jwtData);
+    }
+  }
+
+  /**
+   * @returns {{accessToken: string, refreshToken: string, accessTokenExpAt: number, refreshTokenExpAt: number} | undefined}
+   */
+  get() {
+    return JSON.parse(localStorage.getItem(JWT_KEY));
+  }
+
+  /**
+   * @param {{accessToken: string, refreshToken: string, accessTokenExpAt: number, refreshTokenExpAt: number}} data
+   */
+  #set(data) {
+    const prev = this.get() ?? {};
+    const value = {
+      ...prev,
+      ...data,
+    };
+
+    localStorage.setItem(JWT_KEY, JSON.stringify(value));
+  }
+
+  /**
+   * @param {string} token
+   * @param {number} expAt
+   * Date.now() + 15 * 60 * 1000 - 15min from now
+   */
+  setAccessToken(token, expAt = Date.now() + 15 * 60 * 1000) {
+    this.#set({ accessToken: token, accessTokenExpAt: expAt });
+  }
+
+  /**
+   * @param {string} token
+   * @param {number} expAt
+   * Date.now() + 30 * 24 * 60 * 60 * 1000 - 30days from now
+   */
+  setRefreshToken(token, expAt = Date.now() + 30 * 24 * 60 * 60 * 1000) {
+    this.#set({ refreshToken: token, refreshTokenExpAt: expAt });
+  }
+
+  /**
+   * @param {{accessToken: string, refreshToken: string, accessTokenExpAt: number, refreshTokenExpAt: number}} jwtData
+   */
+  initialize(jwtData) {
+    const accessTokenExpTimeout = jwtData.accessTokenExpAt - Date.now();
+    const refreshTokenExpTimeout = jwtData.refreshTokenExpAt - Date.now();
+
+    if (accessTokenExpTimeout <= 0) {
+      this.notifyAccessExpSubscribers();
+    }
+    if (refreshTokenExpTimeout <= 0) {
+      this.notifyRefreshExpSubscribers();
+    }
+
+    this.#accessTimeout = setTimeout(
+      this.notifyAccessExpSubscribers,
+      accessTokenExpTimeout
+    );
+    this.#refreshTimeout = setTimeout(
+      this.notifyRefreshExpSubscribers,
+      refreshTokenExpTimeout
+    );
+  }
+
+  notifyAccessExpSubscribers() {
+    for (const fn of this.#accessSubscriberFns) {
+      fn();
+    }
+    clearTimeout(this.#accessTimeout);
+  }
+
+  notifyRefreshExpSubscribers() {
+    for (const fn of this.#refreshSubscriberFns) {
+      fn();
+    }
+    clearTimeout(this.#refreshTimeout);
+  }
+
+  /**
+   * @param {function} fn
+   */
+  subscribeAccessExp(fn) {
+    this.#accessSubscriberFns.push(fn);
+  }
+  /**
+   * @param {function} fn
+   */
+  subscribeRefreshExp(fn) {
+    this.#refreshSubscriberFns.push(fn);
+  }
+
+  unsubscribeAll() {
+    clearTimeout(this.#accessTimeout);
+    clearTimeout(this.#refreshTimeout);
+    this.#accessSubscriberFns = [];
+    this.#refreshSubscriberFns = [];
+  }
+}
+
+const instance = new JWTService();
+Object.freeze(instance);
+
+export default instance;
