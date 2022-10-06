dfeb00fb7dae8a97b4fdb3fb93efefd8413a6e81
Isaac Anyimiah <iandev096@gmail.com>
2022-10-06 19:32:27 +0000

    added methods for AuthService - login - logout - isAuthenticated: checks if refreshToken is expired - refreshAuth: gets new accessToken with refreshToken

---
+++ public/js/services/AuthService.js
@@ -0,0 +1,86 @@
+import JWTService from "./JWTService.js";
+
+const API_URL = `https://freddy.codesubmit.io`;
+
+export class AuthService {
+  #jwtService;
+  constructor() {
+    this.#jwtService = JWTService;
+  }
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   */
+  async login(username, password) {
+    try {
+      const response = await fetch(`${API_URL}/login`, {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status !== 200) {
+        throw new Error("Could not login");
+      }
+
+      const data = await response.json();
+
+      this.#jwtService.setAccessToken(data.access_token);
+      this.#jwtService.setRefreshToken(data.refresh_token);
+
+      return data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  logout() {
+    this.#jwtService.clearJwt();
+    this.#jwtService.unsubscribeAccess();
+  }
+
+  isAuthenticated() {
+    const jwtData = this.#jwtService.get();
+    if (!jwtData) {
+      return false;
+    }
+    const refreshTokenIsValid = jwtData.refreshTokenExpAt - Date.now() > 0;
+    return refreshTokenIsValid;
+  }
+
+  async refreshAuth() {
+    const isAuthenticated = this.isAuthenticated();
+    if (isAuthenticated) {
+      try {
+        const refreshToken = this.#jwtService.get().refreshToken;
+        const response = await fetch(`${API_URL}/refresh`, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        });
+
+        if (response.status !== 200) {
+          throw new Error("Could not refresh auth");
+        }
+
+        const data = await response.json();
+        this.#jwtService.setAccessToken(data.access_token);
+
+        return data.access_token;
+      } catch (err) {
+        throw err;
+      }
+    } else {
+      throw new Error("User is not authenticated");
+    }
+  }
+}
+
+const instance = new AuthService();
+Object.freeze(instance);
+
+export default instance;
