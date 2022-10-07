895efc64dddab4a2d4a16d315131880371ffadce
Isaac Anyimiah <iandev096@gmail.com>
2022-10-07 19:08:06 +0000

    moved constants to js folder

--- public/js/constants/error.js
+++ public/js/constants/error.js
@@ -2,4 +2,5 @@ export const ERROR = {
   DATA_FETCH: "Could not fetch data",
   TOKEN_EXPIRED: "Access token expired",
   NOT_AUTHENTICATED: "User not authenticated",
+  REFRESH_FAILED: "Could not refresh auth",
 };
